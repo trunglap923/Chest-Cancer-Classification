c104f0472c60341bf89cf2ec5b8d434f93eccf3c
LapVT2k3 <kendyvu2k3@gmail.com>
2025-01-16 14:25:32 +0700

    feat: Implement model evaluation stage with configuration, component, and pipeline for score calculation and MLflow logging.

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,7 +2,8 @@ from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,
+                                                EvaluationConfig)
 import os
 
 class ConfigurationManager:
@@ -72,3 +73,17 @@ class ConfigurationManager:
         )
 
         return training_config
+
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        training = self.config.training
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")    
+        eval_config = EvaluationConfig(
+            path_of_model=Path(training.trained_model_path),
+            training_data=Path(training_data),
+            mlflow_uri="https://dagshub.com/entbappy/chest-Disease-Classification-MLflow-DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
