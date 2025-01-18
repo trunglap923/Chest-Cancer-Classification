3ea8536c0ef8ae19b5b811e73f4f427a1f56d99d
LapVT2k3 <kendyvu2k3@gmail.com>
2025-01-18 23:40:48 +0700

    feat: add DVC pipeline, model evaluation, and web prediction

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,34 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from cnnClassifier import logger
+import os
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    def predict(self):
+        # load model
+        model = load_model(os.path.join("artifacts","training", "model.h5"))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        test_image = test_image / 255.0 
+
+        prediction_prob = model.predict(test_image, verbose=0)
+        predicted_class_index = np.argmax(prediction_prob, axis=1)[0]
+        confidence_score = prediction_prob[0][predicted_class_index]
+        logger.info(confidence_score)
+
+        if predicted_class_index == 1:
+            prediction = 'Normal'
+            logger.info("Prediction is Normal")
+            return [{"image" : prediction}]
+        else:
+            prediction = 'Adenocarcinoma Cancer'
+            logger.info("Prediction is Adenocarcinoma Cancer")
+            return [{"image" : prediction}]
